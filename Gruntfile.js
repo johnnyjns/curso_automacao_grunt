2167809f9e86ebb3eda769fe8855d91f848a1177
Johnny Souto <johnny.souto@tedbrasil.com>
2017-02-08 15:33:13 -0200

    capitulo-02
    Minificação e/ou cancatenação.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,22 +8,38 @@ module.exports = function(grunt){
                 expand: true,
                 cwd: 'public',
                 src: '**',
-                dest: 'www'
+                dest: 'dist'
             }
         },
 
         clean:{
 
             dist: {
-                src: 'www'
+                src: 'dist'
             }
+        },
+
+        useminPrepare: {
+
+            html: 'dist/**/*.html'
+        },
+
+        usemin: {
+
+            html: 'dist/**/*.html'
         }
 
     });
 
+    grunt.registerTask('minifica', ['useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin']);
     grunt.registerTask('dist', ['clean','copy']);
-    grunt.registerTask('default', ['dist']);
+    grunt.registerTask('default', ['dist', 'minifica']);
 
+    // carregando tasks
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-usemin');
 }
