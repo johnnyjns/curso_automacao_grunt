b66ccd02458181fa20cf090f415ee41aaff31db3
Johnny Souto <johnny.souto@tedbrasil.com>
2017-02-15 15:21:46 -0200

    ponto e virgula faltante

--- Gruntfile.js
+++ Gruntfile.js
@@ -157,4 +157,4 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-browser-sync');
-}
+};
